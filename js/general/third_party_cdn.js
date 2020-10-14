810d9ba357b30b600af77f99b355ade3d3cf89df
Emma <emmaplaysdestiny@gmail.com>
2020-10-14 20:54:11 +0100

    feeds page

--- js/general/third_party_cdn.js
+++ js/general/third_party_cdn.js
@@ -3,7 +3,7 @@ Please note the order of arrangement plays an important role
 */
 /**********************************Jquery CDN ********************************************************/
 document.write(
-    `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>`
+    `<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>`
   );
   
   /**********************************Sweet Alert CDN ********************************************************/
@@ -11,14 +11,14 @@ document.write(
     `<script type='text/javascript' src="https://cdn.jsdelivr.net/npm/sweetalert2" aria-hidden="true"></script>`
   );
   
-  /**********************************Propper.js CDN ********************************************************/
+  /**********************************Popper.js CDN ********************************************************/
   document.write(
-    `<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>`
+    `<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>`
   );
   
   /**********************************Boostrap CDN ********************************************************/
   document.write(
-    `<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>`
+    `<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>`
   );
   
   //*********************************Axios****************************************************************//
