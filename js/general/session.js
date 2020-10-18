1773b97677b1856688132f0afea80d93c1468b7d
junicodefire <junicodefire@gmail.com>
2020-10-18 05:17:21 +0100

    fix

--- js/general/session.js
+++ js/general/session.js
@@ -1,15 +1,5 @@
-/*
-Import Note: This script file should be use to hold data of the current user that is login 
-*/
-//Getting the user information form the browser localStorage
-const onsessionUser = JSON.parse(localStorage.getItem('DevelopND-user'));
-//Check if this user is real or else redirect to login
-let validSession;
-let certified = [];
 
-let action = localStorage.getItem('action');
-
-const checkUser = (token, user) => {
+const checkUser = (token) => {
     //Redirect if not true
     const routes = new Routes();
     const url = `${routes.apiOrigin}${routes.checkSession}`;
@@ -23,26 +13,18 @@ const checkUser = (token, user) => {
     })
     .then(res => {
         if(res) {
-            if(res.status == 401 && action == 0){
+            if(res.status == 401){
                 localStorage.clear();
-                location.replace('../login.html');
-            }else if (res.status == 401 && action == 1) {
-                localStorage.clear();
-                location.replace('../login.html');
-            }else {
-                certified.push(token);
-                certified.push(user)
-
+                location.replace(`${window.location.origin}`);
             }
         }
     })
-
 }
 
-if(onsessionUser){    
-    let {token, user} = onsessionUser;
-    checkUser(token, user);
-}
+const token = localStorage.getItem('@-sorosoke-webapp-token') ? JSON.parse(localStorage.getItem('@-sorosoke-webapp-token'))
+              : null;
+
+token ? checkUser(token) : location.replace(`${window.location.origin}`);
 
 //Get the user info through object destructuring
 
