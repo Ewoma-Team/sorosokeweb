40c18ab076c2dfe168d3d6d2b851c36ab9966ba4
junicodefire <junicodefire@gmail.com>
2020-10-25 23:43:26 +0100

    fix

--- js/brigdeJs/fetchRoomChatBridge.js
+++ js/brigdeJs/fetchRoomChatBridge.js
@@ -4,19 +4,44 @@ const fetchRoomChatBrigde = (e) => {
     e.preventDefault()
 
     const roomName = e.currentTarget.dataset.targetRoom;
+
     //Display room name to the chat view
     document.querySelector('[data-chat-identity]').textContent = roomName;
     document.querySelector('[data-chat-identity-view]').textContent = roomName;
 
+    //View the chat spot on mobile screen
+    roomAndChatViewToggle('room-view') 
+
+    //Store selected room to locatorage to persitense purpose
+    localStorage.setItem('@-SOROSOKE-APP-CURRENT-CHAT-ROOM', JSON.stringify(roomName));
 
 
-    //Store View room to locatorage to persist o
+
     console.log(roomName)
     console.log('hello chat s open')
 
 
 
 
-    //get The Chat 
-    fetchChatApi(e)
+     //get The Chat From Server
+    fetchChatApi()
 }
+
+
+const fetchChatWithPersitRoom = () => {
+
+    const getPersitRoom = localStorage.getItem('@-SOROSOKE-APP-CURRENT-CHAT-ROOM') ? JSON.parse(localStorage.getItem('@-SOROSOKE-APP-CURRENT-CHAT-ROOM')) : null;
+
+        if(getPersitRoom) {
+                document.querySelector('[data-chat-identity]').textContent = getPersitRoom;
+                document.querySelector('[data-chat-identity-view]').textContent = getPersitRoom;
+
+                //View the chat spot on mobile screen
+                roomAndChatViewToggle('room-view') 
+        
+                //get The Chat From Server
+                fetchChatApi()
+        }
+}
+
+fetchChatWithPersitRoom();
--- js/general/roomAndChatViewToggle.js
+++ js/general/roomAndChatViewToggle.js
@@ -2,13 +2,18 @@
 
 const roomAndChatViewToggle = (action) => {
 
-    if(action === 'room-view') {
-        Array.from(document.querySelector('.select-room-section'))[0].style.display = 'none';
-        Array.from(document.querySelector('.chat-view'))[0].style.display = 'block';
-    }
 
-    if(action === 'chat-view') {
-        Array.from(document.querySelector('.chat-view'))[0].style.display = 'none';
-        Array.from(document.querySelector('.select-room-section'))[0].style.display = 'block';
+    var appWindow = window.matchMedia("(max-width: 768px)");
+
+    if (appWindow.matches) {  
+        if(action === 'room-view') {
+            Array.from(document.querySelectorAll('.select-room-section'))[0].style.display = 'none';
+            Array.from(document.querySelectorAll('.chat-view'))[0].style.display = 'block';
+        }
+
+        if(action === 'chat-view') {
+            Array.from(document.querySelectorAll('.chat-view'))[0].style.display = 'none';
+            Array.from(document.querySelectorAll('.select-room-section'))[0].style.display = 'block';
+        }
     }
 }
