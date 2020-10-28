abe557480660130659b4638525fe4769e320fb28
junicodefire <junicodefire@gmail.com>
2020-10-28 22:33:50 +0100

    back to code

--- js/general/apiRoutes.js
+++ js/general/apiRoutes.js
@@ -9,11 +9,11 @@ Import Note: The base_url.js hold every every third party base url origin and ap
 class Routes {
 
     get socketConnection() {
-        return io('https://api-sorosoke.herokuapp.com/');
+        return io('http://127.0.0.1:8200/');
     }
 
     get apiOrigin() {
-      return 'https://api-sorosoke.herokuapp.com/';
+      return 'http://127.0.0.1:8200/';
     }
 
     get apiVersion() {
